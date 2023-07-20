71f96bb6916dbc750d6f060f65b71be6edba9b72
Victor Kataev <kataev.victor.1234@gmail.com>
2023-07-20 19:28:27 +0400

    Add tests

---
+++ tests/Matrix_tests.cpp
@@ -0,0 +1,12 @@
+#include <RobustCV/Matrix.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+
+TEST_CASE("Matrix", "[Matrix]") {
+	SECTION("Default constructor") {
+		rcv::Matrix<int> m;
+		REQUIRE(m.rows() == 0);
+		REQUIRE(m.columns() == 0);
+	}
+}
