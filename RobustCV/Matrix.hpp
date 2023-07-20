6cec1f31390f91e49d63fc25a3140fc1b778559a
Victor Kataev <kataev.victor.1234@gmail.com>
2023-07-20 19:27:31 +0400

    Add Matrix

---
+++ RobustCV/Matrix.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+
+namespace rcv {
+	template <typename T>
+	class Matrix {
+		public:
+			Matrix();
+
+			int rows() const;
+			int columns() const;
+
+		private:
+			std::vector<T> data;
+			int _rows;
+			int _columns;
+	};
+}
+
+namespace rcv {
+	template <typename T>
+	Matrix<T>::Matrix() : _rows(0), _columns(0) {}
+
+	template <typename T>
+	int Matrix<T>::rows() const {
+		return _rows;
+	}
+
+	template <typename T>
+	int Matrix<T>::columns() const {
+		return _columns;
+	}
+}
