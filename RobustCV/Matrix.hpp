f9a4489205be63766b6ed73a05fac61db96ae7b6
Victor Kataev <kataev.victor.1234@gmail.com>
2023-07-22 17:58:01 +0400

    Reformat

--- RobustCV/Matrix.hpp
+++ RobustCV/Matrix.hpp
@@ -3,32 +3,24 @@
 #include <vector>
 
 namespace rcv {
-	template <typename T>
-	class Matrix {
-		public:
-			Matrix();
+template <typename T> class Matrix {
+  public:
+	Matrix();
 
-			int rows() const;
-			int columns() const;
+	int rows() const;
+	int columns() const;
 
-		private:
-			std::vector<T> data;
-			int _rows;
-			int _columns;
-	};
-}
+  private:
+	std::vector<T> data;
+	int _rows;
+	int _columns;
+};
+} // namespace rcv
 
 namespace rcv {
-	template <typename T>
-	Matrix<T>::Matrix() : _rows(0), _columns(0) {}
+template <typename T> Matrix<T>::Matrix() : _rows(0), _columns(0) {}
 
-	template <typename T>
-	int Matrix<T>::rows() const {
-		return _rows;
-	}
+template <typename T> int Matrix<T>::rows() const { return _rows; }
 
-	template <typename T>
-	int Matrix<T>::columns() const {
-		return _columns;
-	}
-}
+template <typename T> int Matrix<T>::columns() const { return _columns; }
+} // namespace rcv
--- tests/Matrix_tests.cpp
+++ tests/Matrix_tests.cpp
@@ -2,9 +2,10 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-
-TEST_CASE("Matrix", "[Matrix]") {
-	SECTION("Default constructor") {
+TEST_CASE("Matrix", "[Matrix]")
+{
+	SECTION("Default constructor")
+	{
 		rcv::Matrix<int> m;
 		REQUIRE(m.rows() == 0);
 		REQUIRE(m.columns() == 0);
